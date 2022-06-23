3dbbb14b3bfe4589065c2a489e4f6d936acbbf93
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-06-23 19:54:56 -0300

    Update establishment_steps.rb

--- features/step_definitions/establishment_steps.rb
+++ features/step_definitions/establishment_steps.rb
@@ -7,12 +7,12 @@ Quando('eu receber o response body da chamada') do
 end
 
 Então('a api irá retornar a chave {string}') do |key|
-  expect(@body[key]).to include 'typeOfEstablishment'
+  expect(@body).to include key
   expect(search.consultation_service.code).to eql 200
   expect(search.consultation_service.message).to eql 'OK'
 end
 
-E('ira exibir um print de um estabelecimento') do
+E('é exibido um print de um estabelecimento') do
   restaurant = search.consultation_service['typeOfEstablishment'][6]['name']
   print "Imprimindo estabelecimento #{restaurant}"
 end
