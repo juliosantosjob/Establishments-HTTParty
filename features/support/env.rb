901bf051c7e61abfcafc5ad8c592ef50c8e9b5e7
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-06-23 18:49:46 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'openssl'
 require 'cucumber'
 require 'rspec'
-require_relative '../services/estabelecimento_services'
+require_relative '../services/establishment_services'
 require_relative '../support/helper'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
