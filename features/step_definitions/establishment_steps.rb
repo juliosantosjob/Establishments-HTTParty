1d279ee8fa761f682d6b728fefc711da0a3fd901
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-06-23 18:50:23 -0300

    Update and rename estabelecimento_steps.rb to establishment_steps.rb

---
+++ features/step_definitions/establishment_steps.rb
@@ -0,0 +1,18 @@
+Dado('que eu faça uma requisição do tipo get no endpoint') do
+  search.consultation_service
+end
+
+Quando('eu receber o response body da chamada') do
+  @body = search.consultation_service.body
+end
+
+Então('a api irá retornar a chave {string}') do |key|
+  expect(@body[key]).to include 'typeOfEstablishment'
+  expect(search.consultation_service.code).to eql 200
+  expect(search.consultation_service.message).to eql 'OK'
+end
+
+E('ira exibir um print de um estabelecimento') do
+  restaurant = search.consultation_service['typeOfEstablishment'][6]['name']
+  print "Imprimindo estabelecimento #{restaurant}"
+end
